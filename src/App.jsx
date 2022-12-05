23ae73ed019a73cfb72a33b3a4aaae8d7e44aaa6
Diego Chagas <drochachagas@gmail.com>
2022-12-05 15:33:23 -0300

    create component Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import styles from './App.module.css'
+import Header from './components/Header'
+
 import './global.css'
 
 function App() {
 
   return (
     <div>
+      <Header />
     </div>
   )
 }
