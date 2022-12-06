c39e87a0afc830ea02e6e6b09d53b3142550fa55
Diego Chagas <drochachagas@gmail.com>
2022-12-06 16:08:58 -0300

    validate form

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,9 @@ import Sidebar from './components/Sidebar'
 
 import styles from './App.module.css'
 import './global.css'
-
-const posts = [
-  {
-    id: '1',
-    author: {
-      avatarUrl: 'https:github.com/diego3g.png',
-      name: 'Diego Fernandes',
-      role: 'CTO @Rocketseat'
-    },
-    content: [
-      { type: 'paragraph', content: 'Fala galeraa 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-      { type: 'link', content: 'jane.design/doctorcare' }
-    ],
-    publishedAt:  new Date('2022-12-03 20:00:00')
-  },
-  {
-    id: '2',
-    author: {
-      avatarUrl: 'https:github.com/maykbrito.png',
-      name: 'Mayk Brito',
-      role: 'Educator @Rocketseat'
-    },
-    content: [
-      { type: 'paragraph', content: 'Fala galeraa 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-      { type: 'link', content: 'jane.design/doctorcare' }
-    ],
-    publishedAt:  new Date('2022-12-06 20:00:00')
-  }
-]
+import { posts } from './api'
 
 function App() {
-
   return (
     <div>
       <Header />
--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { format, formatDistanceToNow } from 'date-fns'
 
 import Avatar from '../../Avatar';
@@ -7,8 +7,35 @@ import Comment from '../Comment';
 import styles from './Post.module.css'
 
 function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState(['Really cool post!'])
+  const [newComment, setNewComment] = useState('')
   const publishedDateFormatted = format(publishedAt, "LLLL d 'at' HH:mm'h'")
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, { addSuffix: true })
+  const isNewCommentEmpty = newComment.length === 0
+
+  function handleCreateNewComment(event) {
+    event.preventDefault()
+
+    setComments([...comments, newComment])
+
+    setNewComment('')
+  }
+
+  function handeNewCommentChange(event) {
+    // event.taget.setCustomValidity('')
+
+    setNewComment(event.target.value)
+  }
+
+  /* function handleNewCommentInvalid(event) {
+    event.target.setCustomValidity('This field is required')
+  } */
+
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment => comment !== commentToDelete)
+    
+    setComments(commentsWithoutDeletedOne)
+  }
 
   return (
     <article className={styles.post}>
@@ -23,38 +50,42 @@ function Post({ author, publishedAt, content }) {
           </div>
         </div>
 
-        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-        {content.map(item => {
-          if(item.type === 'paragraph') return <p>{item.content}</p>
-          else if (item.type === 'link') return <p><a href="#">{item.content}</a></p>
-        })}
-
-        <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a>
-        </p>
+        {content.map(item => (
+          <p key={item.id}>
+            {(item.type === 'paragraph') && item.content}
+            {(item.type === 'link') && <a href="#">{item.content}</a>}
+            {(item.type === 'hashtag') && item.content.map(hashtag => <a key={hashtag.id} href="">#{hashtag.content} </a>)}
+          </p>
+        ))}
       </div>
 
-      <form className={styles.commentForm}>
+      <form className={styles.commentForm} onSubmit={handleCreateNewComment}>
         <strong>Leave your feedback</strong>
 
-        <textarea placeholder="Leave a comment" />
+        <textarea
+          name="comment"
+          placeholder="Leave a comment"
+          value={newComment}
+          onChange={handeNewCommentChange}
+          // onInvalid={handleNewCommentInvalid}
+          required
+        />
 
         <footer>
-          <button type="submit">Publish</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publish
+          </button>
         </footer>
       </form>
 
       <div className={styles.commentLIst}>
-        <Comment />
-        
-        <Comment />
-        
-        <Comment />
+        {comments.map(comment => <Comment key={comment} content={comment} onDeleteComment={deleteComment} />)}
       </div>
     </article>
   );
